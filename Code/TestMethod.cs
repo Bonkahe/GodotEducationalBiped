a66bd407e3be1ec5824b369fde5c9b94267027cb
David House <Bonkahe@gmail.com>
2023-06-19 11:33:18 -0500

    Tutorial 4 animation updates

---
+++ Code/TestMethod.cs
@@ -0,0 +1,10 @@
+using Godot;
+using System;
+
+public partial class TestMethod : Node3D
+{
+	public void NewTestMethod()
+	{
+		Visible = !Visible;
+	}
+}
---
+++ Code/TestMethod.gd
@@ -0,0 +1,5 @@
+extends Node3D
+
+
+func newTestMethod():
+	visible = !visible;
